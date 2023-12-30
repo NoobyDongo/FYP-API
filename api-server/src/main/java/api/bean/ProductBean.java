14cde68e545a4cfac5f3872e84f402d15fb7985a
vinson2580 <122246547+vinson2580@users.noreply.github.com>
2023-12-30 14:42:00 +0800

    Add files via upload

--- api-server/src/main/java/api/bean/ProductBean.java
+++ api-server/src/main/java/api/bean/ProductBean.java
@@ -13,22 +13,29 @@ import java.sql.SQLException;
  * @author Ison Ho
  */
 
+public class ProductBean extends AbstractBean {
 
-public class ProductBean extends AbstractBean{
-    
     public String id;
     public double price;
-    
+    public String name;
+    public String desc;
     public TextBean _name;
     public TextBean _desc;
-    
-    public ProductBean(){
-        
+    public TextBean _producttypeid;
+    public TextBean _originid;
+
+    public int producttype;
+    public int origin;
+
+    public ProductBean() {
+
     }
-    
+
     @Override
-    public void fill(ResultSet rs) throws SQLException{
+    public void fill(ResultSet rs) throws SQLException {
         id = rs.getString("id");
         price = rs.getDouble("price");
+        name = rs.getString("name");
+        desc = rs.getString("desc");
     }
 }
